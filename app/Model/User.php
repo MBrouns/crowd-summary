00919c7417d6f016516fd175477dda8df7f9859a
bouke-nederstigt <bouke.nederstigt@gmail.com>
2014-03-06 11:12:21 +0100

    Started working on User management

---
+++ app/Model/User.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Model class for user management. Taken from cakephp cookbook
+ */
+
+class User extends AppModel {
+    
+    /*
+     * Validate fields before updating databse
+     */
+    public $validate = array(
+        'username' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A username is required'
+            )
+        ),
+        'password' => array(
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'A password is required'
+            )
+        )
+    );
+    
+    /*
+     * Password hashing
+     */
+    public function beforeSave($options = array()) {
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new SimplePasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash(
+                    $this->data[$this->alias]['password']
+            );
+        }
+        return true;
+    }
+
+}
