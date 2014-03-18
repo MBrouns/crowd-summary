51b7cc5145898267e29f8ac504530b4f893654a9
Friso Abcouwer <f.abcouwer@gmail.com>
2014-03-18 23:27:25 +0100

    Fix double hasMany declaration in Document

--- app/Model/Document.php
+++ app/Model/Document.php
@@ -4,13 +4,12 @@ App::uses('AppModel', 'Model');
 
 class Document extends AppModel {
         
-        public $hasMany = array('Sentence', 'Keyword');
     
     public $validate = array(); //todo
 
     /*
      * Define relations
      */
-    public $hasMany = array('PersonalDocument');
+    public $hasMany = array('Sentence', 'Keyword',  'PersonalDocument');
 
 }
