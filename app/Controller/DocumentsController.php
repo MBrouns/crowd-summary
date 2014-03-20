9bdb78e1682fb2b8378e3e46313a155efccc8393
bouke-nederstigt <bouke.nederstigt@gmail.com>
2014-03-20 13:56:37 +0100

    Fixed merge conflict

--- app/Controller/DocumentsController.php
+++ app/Controller/DocumentsController.php
@@ -5,22 +5,22 @@
  */
 
 class DocumentsController extends AppController {
-    
+
     public $uses = array('PersonalDocument', 'Document', 'Summary', 'Sentence');
-    
+
     /*
      * Overview of all documents
      */
 
     public function index() {
-        
+
         if (isset($this->request->data['Document']['file'])) {//file has been uploaded                 
             //save document
             $this->Document->set(array(
                 'title' => $this->request->data['Document']['file']['name'],
                 'fulltext' => file_get_contents($this->request->data['Document']['file']['tmp_name'])
             ));
-            
+
             if ($this->Document->save()) {
                 $this->Session->setFlash('Document was succesfully uploaded');
                 //connect user to document
@@ -28,16 +28,15 @@ class DocumentsController extends AppController {
                     'User' => array('id' => $this->Auth->user('id')),
                     'Document' => array('id' => $this->Document->id),
                 ));
-                
+
 
                 //create summary
                 $document = $this->Document->read(null, $this->Document->id);
                 if (empty($document['Sentence'])) {//generate summary                    
-                    
                     //call java summarizer
                     $cmd = 'java -jar ' . APP . '../summarizers/Summarizer.jar ' . $this->Document->id . ' ' . APP . 'webroot\crowdsum 2>&1'; //some problems with exec in php 5.2.2+ on windows https://bugs.php.net/bug.php?id=41874 check this works on other systems
-                    $lastline = exec($cmd, $output, $returnVar);               
-                    
+                    $lastline = exec($cmd, $output, $returnVar);
+
                     if (count($output) != 6) {//summarizer didn't output all 6 steps so sth is wrong
                         $this->Session->setFlash(__('Automatic summarization failed.'));
                     } else {
@@ -67,10 +66,14 @@ class DocumentsController extends AppController {
         } else {
             $this->set('contentFilter', '');
         }
-       
+
         //get all documents
         $this->set('documents', $this->Document->find('all'));
     }
+
+    /*
+     * Display summary and document to user
+     */
 
     public function summary($id) {
         $this->Document->id = $id;
@@ -78,22 +81,59 @@ class DocumentsController extends AppController {
             throw new NotFoundException(__('Invalid document id'));
         }
 
+        //save summary
+        if ($this->request->is('post')) {
+            debug($this->request);
+        }
+
         //display document
-        $this->set('document', $this->Document->read(null, $this->Document->id));              
-        
+        $this->set('document', $this->Document->read(null, $this->Document->id));
+
         //see if user has personal summary
         $user = $this->Auth->user();
-        $summary = $this->Summary->find('all', array('conditions' => array('user_id' => $user['id'])));        
-        if(!empty($summary)){//user has summary
+        $summary = $this->Summary->find('all', array('conditions' => array('user_id' => $user['id'])));
+        if (!empty($summary)) {//user has summary
             $this->set('personal_summary', $summary);
         }
-        
+
         //Get generated summary @TODO calculate average of all users
         $generated = $this->Summary->find('all', array('conditions' => array('user_id' => 0)));
         $this->set('generated_summary', $generated);
 
         //temp var
         //$this->set('id', $id);
+    }
+
+    /*
+     * Function to generate summary from all user summaries
+     * 
+     * @param int $docId
+     */
+
+    private function generate_summary($docId) {
+        $this->Document->id = $id;
+        if (!$this->Document->exists()) {
+            throw new NotFoundException(__('Invalid document id'));
+        }
+
+
+        //get auto summary
+        $auto_sentences = $this->Summary->find('all', array('conditions' => array('user_id' => 0)));
+
+        //get all users with this docID
+        $users = $this->PersonalDocument->find('all', array('conditions' => array('document_id' => $this->Document->id)));
+
+        if (empty($users)) {//no personal summaries available
+            return $auto_sentences;
+        }
+
+        $total_sentences = count($auto_sentences);
+        foreach ($users as $user) {
+            $summaries[] = $this->Summary->find('all', array('conditions' => array('user_id' => $user['User']['id'])));
+            $total_sentences = $total_sentences + count(end($summaries));
+        }
+
+        $sum_size = $total_sentences / count($users);
     }
 
     /*
--- app/View/Documents/summary.ctp
+++ app/View/Documents/summary.ctp
@@ -1,17 +1,17 @@
 <?php echo $this->Html->script('jquery.textHighlighter.min'); ?>
 <?php echo $this->Html->script('summary'); ?>
-<?php //debug($document); debug($personal_summary); debug($generated_summary);  ?>
+<?php //debug($document); debug($personal_summary); debug($generated_summary);   ?>
 
 <div class="container">
     <div class="summary-container">
         <div class="panel panel-primary">
             <div class="panel-heading"><?php echo $document['Document']['title']; ?></div>
             <div class="panel-body">
-                <p>This document is automatically summarized<?php 
-                if ($document['Document']['contributions'] > 0) {
-                	echo ' and improved by '. $document['Document']['contributions'] .' users';
-                }
-                ?>.</p>
+                <p>This document is automatically summarized<?php
+                    if ($document['Document']['contributions'] > 0) {
+                        echo ' and improved by ' . $document['Document']['contributions'] . ' users';
+                    }
+                    ?>.</p>
                 <div class="btn-group">
                     <button type="button" class="btn btn-default active" id="highlight-button">Highlight</button>
                     <button type="button" class="btn btn-default" id="notes-button">Notes</button>
@@ -24,15 +24,17 @@
         <div id="summary">
             <?php
             foreach ($document['Sentence'] as $sentence) {
-        		echo "<span id='sentence".$sentence['id']."'>" .($sentence['sentence']) .  "</span><br/>";
-   			}  
-   			?>
+                echo "<span id='sentence" . $sentence['id'] . "'>" . ($sentence['sentence']) . "</span><br/>";
+            }
+            ?>
         </div>        
         <?php
         echo $this->Form->create('Summary');
         echo $this->Form->hidden('user_sentences');
         echo $this->Form->submit('Generate', array('class' => 'btn btn-primary right', 'id' => 'generate-button'));
         ?>
+
+        <!-- <button type="submit" class="btn btn-primary right" id="generate-button">Generate</button>  -->
 
         <div class="clearboth"></div>
 
@@ -49,10 +51,10 @@
 </div>
 
 <script type="text/javascript">
-var generated = [];
+    var generated = [];
 <?php
-    foreach ($generated_summary as $sentence) {
-        echo "generated.push('".addslashes($sentence['Summary']['sentence_id']) ."');\n";
-    }
+foreach ($generated_summary as $sentence) {
+    echo "generated.push('" . addslashes($sentence['Summary']['sentence_id']) . "');\n";
+}
 ?>
 </script>
