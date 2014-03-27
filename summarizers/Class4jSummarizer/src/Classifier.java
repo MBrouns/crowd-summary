0702b73e558be944325daa3d1d63df76a946017b
MBrouns <matthijs.brouns@gmail.com>
2014-03-27 13:46:03 +0100

    added getting trainingdata to classifier

--- summarizers/Class4jSummarizer/src/Classifier.java
+++ summarizers/Class4jSummarizer/src/Classifier.java
@@ -1,3 +1,6 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,19 +9,20 @@ import edu.stanford.nlp.classify.LinearClassifierFactory;
 import edu.stanford.nlp.ling.BasicDatum;
 import edu.stanford.nlp.ling.Datum;
 
-
 public class Classifier {
-	
+
 	private LinearClassifier<String, String> classifier;
 	private List<Datum<String, String>> trainingData = new ArrayList<Datum<String, String>>();
 	protected static final String RELEVANT = "relevant";
 	protected static final String NOT_RELEVANT = "notrelevant";
-	
-	
+	private Connection c;
+
 	/**
-	 * Constructs a new classifier and trains it using training data from the database
+	 * Constructs a new classifier and trains it using training data from the
+	 * database
 	 */
-	public Classifier() {
+	public Classifier(Connection c) {
+		this.c = c;
 		createTrainingData();
 		LinearClassifierFactory<String, String> factory = new LinearClassifierFactory<String, String>();
 		factory.useConjugateGradientAscent();
@@ -27,32 +31,60 @@ public class Classifier {
 		// Small amount of smoothing
 		factory.setSigma(10.0);
 		// Build a classifier
-		this.classifier =  factory.trainClassifier(this.trainingData);
+		this.classifier = factory.trainClassifier(this.trainingData);
 	}
-	
+
 	/**
-	 * @return 
+	 * @return
 	 * 
 	 */
-	protected void createTrainingData(){
-		//TODO: Get relevant sentences from db and add to trainingData
-		
-		//TODO: Get irrelevent sentences from db and add to trainingData
-		
-		//this.trainingData.add(makeSentence(, true));
+	protected void createTrainingData() {
+		// TODO: ignore sentences from user_id = 0
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c.setAutoCommit(false);
+			System.out.println("Database connection in classifier established");
+
+			PreparedStatement sqlGetDocuments = c
+					.prepareStatement("SELECT documents.id, COUNT(sentences.id) AS noOfSentences FROM documents LEFT JOIN sentences ON documents.id = sentences.document_id GROUP BY documents.id");
+			ResultSet rsGetDocuments = sqlGetDocuments.executeQuery();
+			while (rsGetDocuments.next()) {
+
+				int noOfRelevantSentences = (int) Math.ceil(Math.sqrt(rsGetDocuments
+						.getInt("noOfSentences")));
+				PreparedStatement sqlGetSentences = c
+						.prepareStatement("SELECT COUNT(users_sentences.id) AS times, sentences.id, sentences.sentence, sentences.document_id FROM users_sentences LEFT JOIN sentences ON sentences.id = users_sentences.sentence_id WHERE sentences.document_id = ? GROUP BY sentences.id ORDER BY times DESC");
+				sqlGetSentences.setInt(1, rsGetDocuments.getInt("id"));
+
+				ResultSet rsGetSentences = sqlGetSentences.executeQuery();
+				int i = 0;
+				while (rsGetSentences.next()) {
+					boolean relevant;
+					if(i < noOfRelevantSentences){
+						relevant = true; 
+					}else{
+						relevant = false;
+					}
+					this.trainingData.add(makeSentence(new ClassifierSentence(rsGetSentences.getInt("id")), relevant));
+					i++;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
 	 * Creates Datum of string that is to be classified
 	 * 
 	 * @param sentence
 	 * @return
 	 */
-	protected  static Datum<String, String> makeSentence(ClassifierSentence sentence) {
+	protected static Datum<String, String> makeSentence(
+			ClassifierSentence sentence) {
 		return makeSentence(sentence, true);
 	}
-	
-	
+
 	/**
 	 * Creates Datum of string that is to be used for training
 	 * 
@@ -60,30 +92,42 @@ public class Classifier {
 	 * @param relevant
 	 * @return
 	 */
-	protected  static Datum<String, String> makeSentence(ClassifierSentence sentence, boolean relevant) {
+	protected static Datum<String, String> makeSentence(
+			ClassifierSentence sentence, boolean relevant) {
 		List<String> features = new ArrayList<String>();
 		// Create content feature
-		features.add("CONTENT=" + sentence);
-		
-		//TODO: Add more features here
+		features.add("CONTENT=" + sentence.getContent());
+		features.add("LENGTH=" + sentence.getLength());
+		features.add("KEYWORDSIM=" + sentence.getKeywordSimilarity());
+		// TODO: Add more features here
 		// Create the label
 		String label = (relevant ? RELEVANT : NOT_RELEVANT);
 		return new BasicDatum<String, String>(features, label);
-		
+
+	}
+
+	/**
+	 * Returns whether sentence is relevant to the summary
+	 * 
+	 * @param sentence
+	 * @return
+	 */
+	public boolean isRelevantSentence(ClassifierSentence sentence) {
+		if (this.classifier.classOf(makeSentence(sentence)).equals(RELEVANT)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	
 	/**
-	 * Returns whether sentence is relevant to the summary
+	 * Returns a relevancy score of the sentence
 	 * @param sentence
 	 * @return
 	 */
-	public boolean isRelevantSentence(ClassifierSentence sentence){
-		if(this.classifier.classOf(makeSentence(sentence)).equals(RELEVANT)){
-			return true;
-		}else{
-			return false;
-		}	
+	public double getSentenceRelevancy(ClassifierSentence sentence){
+		return this.classifier.scoresOf(makeSentence(sentence)).totalCount();
 	}
 
 }
